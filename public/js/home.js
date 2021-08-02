5d7e488559809c8bc75d263460b84ad1bfab70ae
Brian <brianx340@gmail.com>
2021-08-02 20:05:43 -0300

    asd

--- public/js/home.js
+++ public/js/home.js
@@ -28,11 +28,9 @@ window.onload = () => {
         }
 
         if (elementId == 'menu-button') {
-            document.getElementById('menu-mobile').style.display = 'flex'
-            document.getElementById('menu-container').style.display = 'flex'
+            fadeInOutMenu(1)
         } else if (elementId == 'sidebar-close') {
-            document.getElementById('menu-mobile').style.display = 'none'
-            document.getElementById('menu-container').style.display = 'none'
+            fadeInOutMenu(0)
             event.preventDefault()
         } else if (elementId == 'carrito-close') {
             fadeInOutCarrito(0)
@@ -42,9 +40,34 @@ window.onload = () => {
     })
 }
 
-async function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+async function fadeInOutMenu(activo){
+    document.getElementById('menu-mobile').style.display = 'flex'
+    menu = document.getElementById('menu-container')
+    if(activo){
+        opacidadFondo = 0
+        for (i=0 ; i<5 ; i++){
+            opacidadFondo += 0.1
+            document.getElementById('menu-mobile').style.backgroundColor = `rgba(0, 0, 0, ${opacidadFondo})`
+            await sleep(50);
+        }
+        menu.classList.add('menu-in');
+        menu.style.display = 'flex'
+        await sleep(2000);
+        menu.classList.remove('menu-in');
+    } else {
+        menu.classList.add('menu-out');
+        opacidadFondo = 0.5
+        for (i=0 ; i<5 ; i++){
+            opacidadFondo -= 0.1
+            document.getElementById('menu-mobile').style.backgroundColor = `rgba(0, 0, 0, ${opacidadFondo})`
+            await sleep(50);
+        }
+        menu.style.display = 'none'
+        document.getElementById('menu-mobile').style.display = 'none'
+        menu.classList.remove('menu-out');
+    }
 }
+
 
 async function fadeInOutCarrito(activo){
     carrito = document.getElementById('carrito')
@@ -63,4 +86,7 @@ async function fadeInOutCarrito(activo){
         carrito.style.display = 'none'
         carrito.classList.remove('carrito-out');
     }
+}
+async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
