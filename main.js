547ca261332af231d4b46dead977f24dd3afbd7a
Fatima <fatic751@gmail.com>
2021-08-06 00:28:17 -0300

    Product Detail

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const path = require('path')
+const path = require('path');
+const productDetailController = require('./controllers/productDetailController');
 const router = express.Router()
 
 const app = express()
@@ -10,12 +11,13 @@ app.use(express.static(__dirname + '/public'));
 homeRouter = require('./routes/homeRouter')
 formsRouter = require('./routes/formsRouter')
 carritoRouter = require('./routes/carritoRouter')
+productDetailRouter = require('./routes/productDetailRouter')
 
 /* ROUTES */
 app.use('/',homeRouter)
 app.use('/forms',formsRouter)
 app.use('/carrito',carritoRouter)
-
+app.use('/productDetail', productDetailRouter)
 
 app.listen(3000, ()=>{
     console.log('Se esta ejecutando el servidor en localhost:3000')
