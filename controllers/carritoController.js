0be650860f2a9c69b68abc4cdab618ff05b896fe
Brian <brianx340@gmail.com>
2021-08-06 23:18:43 -0300

    asd

--- controllers/carritoController.js
+++ controllers/carritoController.js
@@ -2,6 +2,6 @@ const path = require('path')
 
 module.exports = {
     carrito: (req,res) =>{
-        res.sendFile(path.join(__dirname, '../views/carrito.html'))
+        res.render('templates//carritoPage')
     }
 }
