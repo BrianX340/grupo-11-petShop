d5d42e5569b2e0d29d2c38490cb5f45c87a5822d
Brian <brianx340@gmail.com>
2021-08-06 22:53:35 -0300

    asd

--- routes/productDetailRouter.js
+++ routes/productDetailRouter.js
@@ -2,6 +2,6 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/productDetailController')
 
-router.get('/', controller.forms)
+router.get('/', controller.detail)
 
 module.exports = router
