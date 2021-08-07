843c8fca6ff508f09214dfd4481e60fab4885b65
Brian <brianx340@gmail.com>
2021-08-06 23:05:49 -0300

    asd

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,6 +2,6 @@ const path = require('path')
 
 module.exports = {
     home: (req,res) =>{
-        res.sendFile(path.join(__dirname, '../views/home.html'))
+        res.render('templates//home')
     }
 }
--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const router = express.Router()
 
 const app = express()
 app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
 
 
 /* ROUTERS */
