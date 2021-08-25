72ff191a66daaa7d7dfd7f5f5b0c33a501c82422
Brian <brianx340@gmail.com>
2021-08-25 18:47:10 -0300

    Modificaciones varias

--- public/js/adminPanel.js
+++ public/js/adminPanel.js
@@ -1,5 +1,7 @@
 window.onload = () => {
     document.addEventListener('click', (event)=>{
+
+        //Inicio capturar el id y la clase del elemento
         try {
             elementId = event.path[0].id
             elementClass = event.path[0].className
@@ -8,7 +10,9 @@ window.onload = () => {
             elementId = event.target.id
             elementClass = event.path[0].className
         }
+        //Fin captura
 
+        //Inicio condicional depende del ID
         switch (elementId){
 
             //Inicio casos menu principal
@@ -22,25 +26,47 @@ window.onload = () => {
                 principalViewController('panel-moreOptions')
                 break;
 
-            //Inicio casos menu products
+            //Inicio casos panel menu products
             case 'productsLoaderButton':
-                productViewController('addProductView')
+                panelProductViewController('addProductView')
                 break;
             case 'productsEditButton':
-                productViewController('modifyProductView')
+                panelProductViewController('modifyProductView')
                 break;
             case 'productsDetailButton':
-                productViewController('consultProductView')
+                panelProductViewController('consultProductView')
+                break;
+
+            //Inicio casos panel menu clients
+            case 'clientStatistics':
+                panelClientsViewController('clientStatisticsView')
+                break;
+            case 'dogChat':
+                panelClientsViewController('dogChatView')
+                break;
+            case 'clientsOnline':
+                panelClientsViewController('clientOnlineView')
+                break;
+
+            //Inicio casos panel menu moreOptions
+            case 'transactions':
+                panelMoreViewController('transactionsView')
+                break;
+            case 'statistics':
+                panelMoreViewController('moneyStatisticsView')
+                break;
+            case 'advanceOptions':
+                panelMoreViewController('advanceConfigurationView')
                 break;
 
             
         }
 
         
-
     })
 }
 
+//Funcion de control de vistas raiz
 function principalViewController(idView){
     document.getElementById('panel-menu').style.display = 'none'
     document.getElementById('panel-clients').style.display = 'none'
@@ -50,8 +76,28 @@ function principalViewController(idView){
     document.getElementById(idView).style.display = 'flex'
 }
 
+//Funcion de control de vistas MAS OPCIONES
+function panelMoreViewController(idView){
+    document.getElementById('moreOptionsView').style.display = 'none'
+    document.getElementById('transactionsView').style.display = 'none'
+    document.getElementById('moneyStatisticsView').style.display = 'none'
+    document.getElementById('advanceConfigurationView').style.display = 'none'
 
-function productViewController(idView){
+    document.getElementById(idView).style.display = 'flex'
+}
+
+//Funcion de control de vistas CLIENTES
+function panelClientsViewController(idView){
+    document.getElementById('panelClientView').style.display = 'none'
+    document.getElementById('clientStatisticsView').style.display = 'none'
+    document.getElementById('dogChatView').style.display = 'none'
+    document.getElementById('clientOnlineView').style.display = 'none'
+
+    document.getElementById(idView).style.display = 'flex'
+}
+
+//Funcion de control de vistas PRODUCTOS
+function panelProductViewController(idView){
     document.getElementById('addProductView').style.display = 'none'
     document.getElementById('modifyProductView').style.display = 'none'
     document.getElementById('consultProductView').style.display = 'none'
