ad5eff784b25763db70a94b51ecf098413256e58
Brian <brianx340@gmail.com>
2021-09-02 19:17:06 -0300

    creacion rama para hacer pruebas

--- src/main.js
+++ src/main.js
@@ -33,5 +33,5 @@ app.use((req,res, next)=>{
 
 
 app.listen(3000, ()=>{
-    console.log('Se esta ejecutando el servidor en localhost:3000')
+    console.log('Se esta ejecutando el servidor en: localhost:3000')
 })
