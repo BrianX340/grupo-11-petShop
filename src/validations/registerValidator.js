a14d7f91d7af8a560bb5b791fbb93e4e8ed2ddeb
Fatima <fatic751@gmail.com>
2021-09-06 17:52:25 -0300

    varegis

--- src/validations/registerValidator.js
+++ src/validations/registerValidator.js
@@ -1,29 +1,13 @@
 const {check, body} = require('express-validator');
+const { getUsers } = require('../database/db');
 
-
-module.exports = [
-
-
-    check('user1')
+module.exports = 
+[
+    check('userName')
     .notEmpty()
-    .withMessage('Debes escribir el usuario'),
-
-    check('name1')
-    .notEmpty()
-    .withMessage('Debes escribir un Nombre'),
-
-    check('last_name1')
-    .notEmpty()
-    .withMessage('Debes escribir un Apellido'),
-
-    check('email1')
-    .notEmpty()
-    .withMessage('Debes escribir un email').bail()
-    .isEmail()
-    .withMessage('Debes escribir un email válido'),
-    body('email')
+    .withMessage('Debes escribir un usuario')
     .custom(value => {
-        let user = users.find(user => user.email === value);
+        let user = getUsers().find(user => user.userName === value)
 
         if(user === undefined){
             return true
@@ -31,23 +15,48 @@ module.exports = [
             return false
         }
     })
-    .withMessage("Correo no registrado"),
+    .withMessage("usuario en uso"),
 
-    check('pass1')
+    check('name')
     .notEmpty()
-    .withMessage('Debes escribir tu contraseña')
+    .withMessage('Debes escribir un nombre'),
+
+    check('last_name')
+    .notEmpty()
+    .withMessage('Debes escribir un apellido'),
+
+    check('email')
+    .notEmpty()
+    .withMessage('Debes escribir un email').bail()
+    .isEmail()
+    .withMessage('Debes escribir un email válido'),
+
+    body('email')
+    .custom(value => {
+        let user = getUsers().find(user => user.email === value)
+
+        if(user === undefined){
+            return true
+        }else{
+            return false
+        }
+    })
+    .withMessage("Email ya registrado"),
+
+    check('pass')
+    .notEmpty()
     .isLength({
         min: 6,
         max: 20
     })
-    .withMessage('La contraseña debe tener entre 6 y 20 caracteres'),
+    .withMessage('La contraseña debe tener como mínimo de 6 a 20 caracteres'),
 
     body('pass2')
-    .custom((value, {req}) => value !== req.body.pass1 ? false : true)
-    .withMessage('Las contraseñas no coinciden'),
-
-    check('terms')
+    .custom((value, {req}) => value !== req.body.pass ? false : true)
+    .withMessage('Las contraseñas no coinciden')
+    
+    /* check('terms')
     .isString('on')
-    .withMessage('Debes aceptar los términos y condiciones')
+    .withMessage('Debes aceptar los términos y condiciones') */
 
 ]
