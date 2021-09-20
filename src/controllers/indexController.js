f6de2e0cc9cd530e1886e44077aa472c6d308103
Brian <brianx340@gmail.com>
2021-09-20 18:58:09 -0300

    asd

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -8,14 +8,14 @@ module.exports = {
             favorites: [],
             bestSells:info,
             promotions:info,
-            user : req.session.user ? req.session.user : '',
+            session : req.session ? req.session : '',
             favorites: req.session.user ? req.session.user.favorites : '',
             carrouselImages:[
                 "/img/banersCarrousel/banner01.jpg"
             ]
         }
         
-        res.render('index//home',{data, session: req.session.user ? req.session.user : ""})
+        res.render('index//home',{data})
     },
 
 /*     productDetail: (req,res) =>{
@@ -36,7 +36,7 @@ module.exports = {
             search
         }
 
-        res.render('index//searchPage', {data, session: req.session.user ? req.session.user : ""} )
+        res.render('index//searchPage', {data, session: req.session ? req.session : ""} )
 
 
     },
@@ -48,7 +48,7 @@ module.exports = {
 
         res.render('index//productDetail', {
             product,
-            session: req.session.user ? req.session.user : ""
+            session: req.session ? req.session : ""
         })
     }
 }
